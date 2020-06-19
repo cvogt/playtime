1eba593afef38710ab1c63d5bf8fbab294b2a165
Christopher Vogt <oss.nsp@cvogt.org>
2020-06-18 22:30:01 -0400

    add some midi music for fun

--- lib/Music.hs
+++ lib/Music.hs
@@ -12,9 +12,22 @@ spawnSynth =
 
 playMusic :: IO ()
 playMusic = do
-  let p1 = [c 5 qn, e 5 qn, d 5 en, c 5 en, e 5 qn, c 5 qn, b 4 en, e 5 en, a 4 hn]
-  let p2 = [e 5 qn, e 5 qn, g 5 qn, g 5 en, a 5 en, f 5 qn, a 5 qn, e 5 hn]
-  let p3 = [c 6 qn, b 5 en, a 5 en, b 5 qn, e 5 qn, a 5 qn, e 5 en, d 5 en, e 5 qn, b 4 qn]
   let p = Euterpea.play . Euterpea.line
-  p $ p1 <> p2 <> p3
-  p $ [chord [c 4 en, e 4 en, g 4 en], chord [c 4 en, e 4 en, g 4 en], chord [c 4 en, e 4 en, g 4 en], chord [a 3 hn, c 4 hn, e 4 hn]] -- p1 <> p2 <> p3
+  -- let p1 = [c 5 qn, e 5 qn, d 5 en, c 5 en, e 5 qn, c 5 qn, b 4 en, e 5 en, a 4 hn]
+  -- let p2 = [e 5 qn, e 5 qn, g 5 qn, g 5 en, a 5 en, f 5 qn, a 5 qn, e 5 hn]
+  -- let p3 = [c 6 qn, b 5 en, a 5 en, b 5 qn, e 5 qn, a 5 qn, e 5 en, d 5 en, e 5 qn, b 4 qn]
+  -- p $ p1 <> p2 <> p3
+  -- p $ [chord [c 4 en, e 4 en, g 4 en], chord [c 4 en, e 4 en, g 4 en], chord [c 4 en, e 4 en, g 4 en], chord [a 3 hn, c 4 hn, e 4 hn]] -- p1 <> p2 <> p3
+  let loykrathong =
+        [c 3 en, c 3 en, a 2 en, c 3 en, d 3 en, f 3 qn] <> [g 3 en, f 3 en, d 3 qn, c 3 en, d 3 en, c 3 en, a 2 qn]
+          <> [c 3 en, d 3 en, f 3 en, d 3 en, f 3 qn]
+          <> [c 3 en, d 3 en, f 3 en, g 3 qn, f 3 en, d 3 en, c 3 en, c 3 qn]
+          <> [f 3 en, f 3 en, f 3 en, f 3 en, f 3 en, f 3 qn]
+          <> [c 3 en, c 3 en, c 3 en, c 3 en, c 3 en, c 3 qn]
+          <> [f 3 en, f 3 en, f 3 en, g 3 en, a 3 qn]
+          <> [g 3 en, f 3 en, d 3 en, c 3 en, d 3 en, f 3 en, g 3 en, a 3 qn]
+          <> [a 3 en, a 3 en, a 3 en, a 3 en, a 3 en, a 3 qn]
+          <> [f 3 en, f 3 en, f 3 en, f 3 en, f 3 en, f 3 qn]
+          <> [f 3 en, d 3 en, c 3 en, d 3 en, f 3 en, d 3 en, f 3 qn]
+          <> [f 3 en, d 3 en, c 3 en, d 3 en, f 3 en, d 3 en, f 3 qn]
+  p $ tempo 0.75 <$> loykrathong

f1afc8e83eea67df88135c4d529ee483ef7a7466
Jan Braun <janbraun@gmx.net>
2020-07-27 09:08:09 +0200

    Improve debug output terminal redraw handling.
    Other things written to stdout might still get mangled by the constant debug
    output. But at least it will be visible that other output appeared. And
    it'll ilikely be readable, too. We just can't guarantee that.

--- lib/Playtime/Debug.hs
+++ lib/Playtime/Debug.hs
@@ -5,6 +5,7 @@ import Data.Aeson
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.HashMap.Strict as HMS
 import Data.List (lines, zip)
+import qualified Data.List as DL
 import qualified Data.Text as T
 import GHC.Float (int2Double)
 import GHC.Real ((/), round)
@@ -35,7 +36,6 @@ trackTime mvar = void . trackTimeM mvar . pure
 
 forkDebugTerminal :: ConcurrentState -> MVar EngineConfig -> Maybe LiveCodeState -> IO ThreadId
 forkDebugTerminal ConcurrentState {..} engineConfigMVar lcsMay = do
-  -- FIXME: cursor stays hidden after termination
   forkIO $ do
     flip iterateM_ (0, 0, 0, 0) $ \(oldAvgTimeStep, oldAvgTexturePlacementTime, oldAvgRenderLoopTime, oldAvgTotalLoopTime) -> do
       engineState@EngineState {..} <- readMVar csEngineState
@@ -47,33 +47,34 @@ forkDebugTerminal ConcurrentState {..} engineConfigMVar lcsMay = do
           newAvgTexturePlacementTime = if not $ null texturePlacementTimes then (/ 10) . int2Double . round @Double @Int $ 10 * 1 / (pico2second $ avg $ uncurry timeDiffPico <$> texturePlacementTimes) else oldAvgTexturePlacementTime
           newAvgRenderLoopTime = if not $ null renderLoopTimes then (/ 10) . int2Double . round @Double @Int $ 10 * 1 / (pico2second $ avg $ uncurry timeDiffPico <$> renderLoopTimes) else oldAvgRenderLoopTime
           newAvgTotalLoopTime = if not $ null totalLoopTimes then (/ 10) . int2Double . round @Double @Int $ 10 * 1 / (pico2second $ avg $ uncurry timeDiffPico <$> totalLoopTimes) else oldAvgTotalLoopTime
+          display =
+            [ "fps: " <> show newAvgTotalLoopTime,
+              "1/renderLoopTime: " <> show newAvgRenderLoopTime,
+              "1/texturePlacementTime: " <> show newAvgTexturePlacementTime,
+              "1/timeStep: " <> show newAvgTimeStep,
+              DL.repeat '-',
+              "esCursorPos: " <> show esCursorPos,
+              "esKeysPressed: " <> show esKeysPressed,
+              "esMousePressed: " <> show esMousePressed,
+              "esActions: " <> show esActions,
+              "esTimePassed: " <> show esTimePassed,
+              DL.repeat '-'
+            ]
+
       gameDebugInfo <- ecGameDebugInfo <$> readMVar engineConfigMVar
       gameInfo <- gameDebugInfo engineState
-      clearFromCursorToScreenBeginning
-      restoreCursor
-      saveCursor
+
       (join . join -> ce) <- sequence $ tryReadMVar . lcsCompileError <$> lcsMay
       Just TerminalSize.Window {height, width} <- TerminalSize.size
       putStrLn $ T.unpack $ T.unlines $ take (height -2) $ join $
         T.chunksOf width . T.stripEnd . T.pack
-          <$> (maybe [] (\e -> lines $ setSGRCode [SetColor Foreground Vivid Red] <> e <> "\n" <> replicate width '-' <> setSGRCode [ANSI.Reset]) ce)
-          <> ( take width
-                 <$> [ "fps: " <> show newAvgTotalLoopTime,
-                       "1/renderLoopTime: " <> show newAvgRenderLoopTime,
-                       "1/texturePlacementTime: " <> show newAvgTexturePlacementTime,
-                       "1/timeStep: " <> show newAvgTimeStep,
-                       replicate width '-',
-                       "esCursorPos: " <> show esCursorPos,
-                       "esKeysPressed: " <> show esKeysPressed,
-                       "esMousePressed: " <> show esMousePressed,
-                       "esActions: " <> show esActions,
-                       "esTimePassed: " <> show esTimePassed,
-                       replicate width '-'
-                     ]
-                 <> gameInfo
-             )
-
+          <$> (maybe [] (\e -> lines $ setSGRCode [SetColor Foreground Vivid Red] <> e <> "\n" <> replicate (width -1) '-' <> setSGRCode [ANSI.Reset]) ce)
+          <> (take (width -1) <$> display <> gameInfo)
+      cursorUp $ (length $ display <> gameInfo) + 1 -- trailing newline
+      setCursorColumn 0
       threadDelay $ 200 * 1000 -- FIXME: changing this to 100 * make process freeze on exit
+      clearFromCursorToScreenEnd
+
       pure (oldAvgTimeStep, newAvgTexturePlacementTime, newAvgRenderLoopTime, newAvgTotalLoopTime)
 
 debugPrint :: ToJSON a => a -> [[Char]]

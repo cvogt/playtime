4f6ecc598e882ca23bfdadb46686facdac98bd0a
Marc Wu <github@marcwu.de>
2020-08-16 23:45:04 +0200

    player movement, screen bounds, enemy placement

--- multispace/MultiSpace/Game.hs
+++ multispace/MultiSpace/Game.hs
@@ -5,17 +5,21 @@
 module MultiSpace.Game where
 
 import Data.Aeson (FromJSON, ToJSON)
+import GHC.Err (error)
 import My.Prelude
 import Playtime
 
-data TextureId = Enemy deriving (Eq, Ord, Show, Data, Bounded, Enum, Generic, NFData, ToJSON, FromJSON)
+data TextureId = Enemy | Player
+  deriving (Eq, Ord, Show, Data, Bounded, Enum, Generic, NFData, ToJSON, FromJSON)
 
 textures :: TextureId -> (Scale, FilePath)
 textures = \case
+  Player -> (0.3, "enemy_red.png")
   Enemy -> (0.1, "enemy_red.png")
 
 data GameState = GameState
-  { gsPlayer :: Pos
+  { gsPlayer :: Pos,
+    gsEnemy :: Pos
   }
   deriving (Show, Generic, NFData, ToJSON, FromJSON)
 
@@ -24,7 +28,8 @@ makeInitialGameState dimensions seed =
   let rng = mkStdGen seed
       (poss, _) = randomPoss rng 99 dimensions
    in GameState
-        { gsPlayer = (10000, 10000)
+        { gsPlayer = dimensions / 2,
+          gsEnemy = fromMaybe (error "no enemy position") $ headMay poss
         }
 
 stepGameStatePure :: Int -> (TextureId -> Dim) -> GameState -> EngineState -> Event -> GameState
@@ -33,10 +38,23 @@ stepGameStatePure seed tDim gs@GameState {..} EngineState {..} = \case
     gs
   RenderEvent _ ->
     let rng = mkStdGen seed
-     in gs
+        speed = 200
+        bounds = esDimensions - tDim Player
+        delta =
+          (if Key'Up `setMember` esKeysPressed then (0, - speed * esTimePassed) else 0)
+            + (if Key'Down `setMember` esKeysPressed then (0, speed * esTimePassed) else 0)
+            + (if Key'Left `setMember` esKeysPressed then (- speed * esTimePassed, 0) else 0)
+            + (if Key'Right `setMember` esKeysPressed then (speed * esTimePassed, 0) else 0)
+     in gs {gsPlayer = maxPairWise 0 $ minPairWise bounds $ gsPlayer + delta}
   _ -> gs
 
 visualize :: (TextureId -> Pos -> Sprite) -> EngineState -> GameState -> [Sprite]
 visualize sprite EngineState {..} GameState {..} =
   let
-   in []
+   in [sprite Player gsPlayer, sprite Enemy gsEnemy]
+
+minPairWise :: (Double, Double) -> (Double, Double) -> (Double, Double)
+minPairWise = pairWise min min
+
+maxPairWise :: (Double, Double) -> (Double, Double) -> (Double, Double)
+maxPairWise = pairWise max max
